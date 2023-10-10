f3bf4070a12c59fcd8cd233f63cc3cb693553012
davidct6 <54284205+davidct6@users.noreply.github.com>
2023-10-10 16:15:40 +0200

    Add files via upload

---
+++ rfidElechouse.py
@@ -0,0 +1,21 @@
+from pynfc import Nfc, Desfire, Timeout
+
+N = Nfc("pn532_uart:/dev/ttyAMA0:115200")
+DESFIRE_DEFAULT_KEY = b'\x00'*8
+MIFARE_BLANK_TOKEN =b'xFF'*1024*4
+
+print("Apropeu targeta")
+
+class rfidElechouse:
+    def read_uid(self):
+        for target in n.poll():
+            try:
+                uid = target.uid.decode().upper()
+                return uid
+            except TimeoutException:
+                pass
+
+if __name__ == "__main__":
+    rf = rfidElechouse()
+    uid =rf.read_uid()
+    print(uid)
